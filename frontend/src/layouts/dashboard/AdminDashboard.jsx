1905f55920560de4b64f1f183650fa9280bf26e0
costin <chgeorghe0702@stud.acs.upb.ro>
2021-12-10 13:36:17 +0200

    add Comenzi

--- frontend/src/layouts/dashboard/AdminDashboard.jsx
+++ frontend/src/layouts/dashboard/AdminDashboard.jsx
@@ -13,6 +13,7 @@ import { Link } from 'react-router-dom'
 import Profile from '../../components/Profile'
 import Produse from '../dashboardRoutes/Produse'
 import Categorii from '../dashboardRoutes/Categorii'
+import Comenzi from '../dashboardRoutes/Comenzi'
 
 function AdminDashobard() {
     const { user: currentUser } = useSelector((state) => state.auth)
@@ -87,30 +88,33 @@ function AdminDashobard() {
                     admin={true}
                 />
                 <div className="content">
-                    <div className="nav">
-                        <div className="departament">
-                            Departamentul de Achizitii
-                        </div>
-                        <Link
-                            to="/admin/dashboard/profil"
-                            style={{
-                                textDecoration: 'none',
-                                color: '#e1e1e1',
-                            }}
-                        >
-                            <div className="right">
-                                <p>{`${currentUser.nume} ${currentUser.prenume}`}</p>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M20.822 18.096c-3.439-.794-6.641-1.49-5.09-4.418 4.719-8.912 1.251-13.678-3.732-13.678-5.081 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-2.979.688-3.178 2.143-3.178 4.663l.005 1.241h10.483l.704-3h1.615l.704 3h10.483l.005-1.241c.001-2.52-.198-3.975-3.177-4.663zm-8.231 1.904h-1.164l-.91-2h2.994l-.92 2z" />
-                                </svg>
+                    {activeTabName === 'Profil' && (
+                        <div className="nav">
+                            <div className="departament">
+                                Departamentul de Achizitii
                             </div>
-                        </Link>
-                    </div>
+                            <Link
+                                to="/admin/dashboard/profil"
+                                style={{
+                                    textDecoration: 'none',
+                                    color: '#e1e1e1',
+                                }}
+                            >
+                                <div className="right">
+                                    <p>{`${currentUser.nume} ${currentUser.prenume}`}</p>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="24"
+                                        height="24"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path d="M20.822 18.096c-3.439-.794-6.641-1.49-5.09-4.418 4.719-8.912 1.251-13.678-3.732-13.678-5.081 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-2.979.688-3.178 2.143-3.178 4.663l.005 1.241h10.483l.704-3h1.615l.704 3h10.483l.005-1.241c.001-2.52-.198-3.975-3.177-4.663zm-8.231 1.904h-1.164l-.91-2h2.994l-.92 2z" />
+                                    </svg>
+                                </div>
+                            </Link>
+                        </div>
+                    )}
+
                     <div className="innerContent">
                         <Switch>
                             <Route path="/admin/dashboard/furnizori">
@@ -120,7 +124,7 @@ function AdminDashobard() {
                                 <Facturi />
                             </Route>
                             <Route path="/admin/dashboard/comenzi">
-                                <h1>Comenzi</h1>
+                                <Comenzi />
                             </Route>
                             <Route path="/admin/dashboard/angajati">
                                 <Angajati />
---
+++ frontend/src/layouts/dashboardRoutes/Comenzi.jsx
@@ -0,0 +1,187 @@
+function Comenzi() {
+    return (
+        <div className="ordersContainer">
+            <div className="nav">
+                <h1>6 Comenzi</h1>
+                <div className="addNewOrder">Plasati Comanda</div>
+            </div>
+            <div className="ordersInner">
+                <div className="order">
+                    <div className="topOrderContainer">
+                        <div className="topOrderLeft">
+                            <div className="box">
+                                <p className="heading">Comanda Plasata</p>
+                                <p className="subheading">Oct 27, 2019</p>
+                            </div>
+                            <div className="box">
+                                <p className="heading">Total</p>
+                                <p className="subheading">120.00</p>
+                            </div>
+                        </div>
+                        <div className="topOrderRight">
+                            <div className="box">
+                                <p className="heading">Comanda #123767</p>
+                                <p className="subheading">Detalii Comanda</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="middleOrderContainer">
+                        <div className="middleLeft">
+                            <div className="image">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M22 4h-20c-1.104 0-2 .896-2 2v12c0 1.104.896 2 2 2h20c1.104 0 2-.896 2-2v-12c0-1.104-.896-2-2-2zm-19 5.78c0-.431.349-.78.78-.78h.428v1.125h-1.208v-.345zm0 .764h1.208v.968h-1.208v-.968zm0 1.388h1.208v1.068h-.428c-.431 0-.78-.349-.78-.78v-.288zm3 5.068h-3v-1h3v1zm1-4.78c0 .431-.349.78-.78.78h-.429v-1.068h1.209v.288zm0-.708h-1.209v-.968h1.209v.968zm0-1.387h-1.629v2.875h-.743v-4h1.592c.431 0 .78.349.78.78v.345zm4 6.875h-3v-1h3v1zm1-6.5c0-1.381 1.119-2.5 2.5-2.5.484 0 .937.138 1.32.377-.531.552-.857 1.3-.857 2.123 0 .824.327 1.571.857 2.123-.383.239-.836.377-1.32.377-1.381 0-2.5-1.119-2.5-2.5zm4 6.5h-3v-1h3v1zm5 0h-3v-1h3v1zm-2.5-4c-1.38 0-2.5-1.119-2.5-2.5s1.12-2.5 2.5-2.5c1.381 0 2.5 1.119 2.5 2.5s-1.119 2.5-2.5 2.5z" />
+                                </svg>
+                            </div>
+                            <div className="text">
+                                <h1>Detalii Furnizor</h1>
+
+                                <p>Doraly S.R.L.</p>
+                                <p>
+                                    - Bucuresti, Bucuresti, Strada Gloriei, 23
+                                </p>
+                                <p>- Nr. Telefon: 0987654321</p>
+                            </div>
+                        </div>
+                        <div className="middleRight">
+                            <div className="btn-solid">Vizualizare</div>
+                            <div
+                                className="btn-outlined"
+                                style={{ marginTop: '20px' }}
+                            >
+                                Vizualizare
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottomOrderContainer">
+                        <p>
+                            Livrare estimata: <span>Nov 15, 2019</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="order">
+                    <div className="topOrderContainer">
+                        <div className="topOrderLeft">
+                            <div className="box">
+                                <p className="heading">Comanda Plasata</p>
+                                <p className="subheading">Oct 27, 2019</p>
+                            </div>
+                            <div className="box">
+                                <p className="heading">Total</p>
+                                <p className="subheading">120.00</p>
+                            </div>
+                        </div>
+                        <div className="topOrderRight">
+                            <div className="box">
+                                <p className="heading">Comanda #123767</p>
+                                <p className="subheading">Detalii Comanda</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="middleOrderContainer">
+                        <div className="middleLeft">
+                            <div className="image">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M22 4h-20c-1.104 0-2 .896-2 2v12c0 1.104.896 2 2 2h20c1.104 0 2-.896 2-2v-12c0-1.104-.896-2-2-2zm-19 5.78c0-.431.349-.78.78-.78h.428v1.125h-1.208v-.345zm0 .764h1.208v.968h-1.208v-.968zm0 1.388h1.208v1.068h-.428c-.431 0-.78-.349-.78-.78v-.288zm3 5.068h-3v-1h3v1zm1-4.78c0 .431-.349.78-.78.78h-.429v-1.068h1.209v.288zm0-.708h-1.209v-.968h1.209v.968zm0-1.387h-1.629v2.875h-.743v-4h1.592c.431 0 .78.349.78.78v.345zm4 6.875h-3v-1h3v1zm1-6.5c0-1.381 1.119-2.5 2.5-2.5.484 0 .937.138 1.32.377-.531.552-.857 1.3-.857 2.123 0 .824.327 1.571.857 2.123-.383.239-.836.377-1.32.377-1.381 0-2.5-1.119-2.5-2.5zm4 6.5h-3v-1h3v1zm5 0h-3v-1h3v1zm-2.5-4c-1.38 0-2.5-1.119-2.5-2.5s1.12-2.5 2.5-2.5c1.381 0 2.5 1.119 2.5 2.5s-1.119 2.5-2.5 2.5z" />
+                                </svg>
+                            </div>
+                            <div className="text">
+                                <h1>Detalii Furnizor</h1>
+
+                                <p>Doraly S.R.L.</p>
+                                <p>
+                                    - Bucuresti, Bucuresti, Strada Gloriei, 23
+                                </p>
+                                <p>- Nr. Telefon: 0987654321</p>
+                            </div>
+                        </div>
+                        <div className="middleRight">
+                            <div className="btn-solid">Vizualizare</div>
+                            <div
+                                className="btn-outlined"
+                                style={{ marginTop: '20px' }}
+                            >
+                                Vizualizare
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottomOrderContainer">
+                        <p>
+                            Livrare estimata: <span>Nov 15, 2019</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="order">
+                    <div className="topOrderContainer">
+                        <div className="topOrderLeft">
+                            <div className="box">
+                                <p className="heading">Comanda Plasata</p>
+                                <p className="subheading">Oct 27, 2019</p>
+                            </div>
+                            <div className="box">
+                                <p className="heading">Total</p>
+                                <p className="subheading">120.00</p>
+                            </div>
+                        </div>
+                        <div className="topOrderRight">
+                            <div className="box">
+                                <p className="heading">Comanda #123767</p>
+                                <p className="subheading">Detalii Comanda</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="middleOrderContainer">
+                        <div className="middleLeft">
+                            <div className="image">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path d="M22 4h-20c-1.104 0-2 .896-2 2v12c0 1.104.896 2 2 2h20c1.104 0 2-.896 2-2v-12c0-1.104-.896-2-2-2zm-19 5.78c0-.431.349-.78.78-.78h.428v1.125h-1.208v-.345zm0 .764h1.208v.968h-1.208v-.968zm0 1.388h1.208v1.068h-.428c-.431 0-.78-.349-.78-.78v-.288zm3 5.068h-3v-1h3v1zm1-4.78c0 .431-.349.78-.78.78h-.429v-1.068h1.209v.288zm0-.708h-1.209v-.968h1.209v.968zm0-1.387h-1.629v2.875h-.743v-4h1.592c.431 0 .78.349.78.78v.345zm4 6.875h-3v-1h3v1zm1-6.5c0-1.381 1.119-2.5 2.5-2.5.484 0 .937.138 1.32.377-.531.552-.857 1.3-.857 2.123 0 .824.327 1.571.857 2.123-.383.239-.836.377-1.32.377-1.381 0-2.5-1.119-2.5-2.5zm4 6.5h-3v-1h3v1zm5 0h-3v-1h3v1zm-2.5-4c-1.38 0-2.5-1.119-2.5-2.5s1.12-2.5 2.5-2.5c1.381 0 2.5 1.119 2.5 2.5s-1.119 2.5-2.5 2.5z" />
+                                </svg>
+                            </div>
+                            <div className="text">
+                                <h1>Detalii Furnizor</h1>
+
+                                <p>Doraly S.R.L.</p>
+                                <p>
+                                    - Bucuresti, Bucuresti, Strada Gloriei, 23
+                                </p>
+                                <p>- Nr. Telefon: 0987654321</p>
+                            </div>
+                        </div>
+                        <div className="middleRight">
+                            <div className="btn-solid">Vizualizare</div>
+                            <div
+                                className="btn-outlined"
+                                style={{ marginTop: '20px' }}
+                            >
+                                Vizualizare
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottomOrderContainer">
+                        <p>
+                            Livrare estimata: <span>Nov 15, 2019</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Comenzi
