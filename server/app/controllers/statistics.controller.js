976ff2bbaf2b3bd4e0ddef6194e6c8b699fb4e06
costin <chgeorghe0702@stud.acs.upb.ro>
2022-01-14 10:15:09 +0200

    added more products and fixed some queries

--- server/app/controllers/statistics.controller.js
+++ server/app/controllers/statistics.controller.js
@@ -8,14 +8,13 @@ exports.totalSpentByMonth = (req, res) => {
   const monthsNo = req.params.numberOfMonths;
 
   let query =
-    "select distinct month(c.data_comanda) as luna,  " +
+    "select distinct month(c.data_comanda) as luna, " +
     "(select sum(p.pret * pc.cantitate) " +
-    "from produse p inner join produsecomenzi pc on p.cod_produs = pc.cod_produs " +
-    "where month(c.data_comanda) = month(pc.data_comanda)  ) as total " +
-    "from produsecomenzi pc, comenzi c " +
-    `where c.data_comanda > curdate() - interval (dayofmonth(curdate()) - 1) day - interval ${monthsNo} month ` +
-    "order by month(c.data_comanda) desc ";
-
+    " from produse p inner join produsecomenzi pc on p.cod_produs = pc.cod_produs " +
+    "inner join comenzi c2 on c2.nr_comanda = pc.nr_comanda " +
+    `where month(c2.data_comanda) = month(c.data_comanda) and c.data_comanda > curdate() - interval (dayofmonth(curdate()) - 1) day - interval ${monthsNo} month ) as total ` +
+    "from comenzi c " +
+    "order by month(c.data_comanda) desc; ";
   db.query(query, async (error, results) => {
     if (error) {
       console.log(error);
@@ -68,8 +67,10 @@ exports.totalSpentByMonth = (req, res) => {
 
       results.map((res) => {
         for (let i = 0; i < last_x_months.length; i++)
-          if (res.luna - 1 === data[i].index_luna) data[i].total = res.total;
+          if (res.luna - 1 === data[i].index_luna)
+            data[i].total = res.total ? res.total : 0;
       });
+
       return res.json({
         message: "Success!",
         status: 200,
